3b9cf5ce6950f632b5aec2aa2dfc885005d01b67
CristianMorea <120284574+CristianMorea@users.noreply.github.com>
2024-05-20 11:38:12 -0500

    registro completo

--- Backend/server.js
+++ Backend/server.js
@@ -60,20 +60,69 @@ app.get('/articles', (req, res) => {
   });
 });
 
-// Endpoint para registrar usuario
+
+
+
+// Función para insertar usuario en la base de datos
 const insertUsuarioRegistro = (usuario, callback) => {
   const { nombre, correo, contraseña } = usuario;
-  const query = `
-    INSERT INTO usuario(nombre, correo, contraseña, rol_id, fecha_creacion)
-    VALUES (?, ?, ?, 1, CURRENT_TIMESTAMP)
-  `;
-  connection.query(query, [nombre, correo, contraseña], (err, results) => {
+
+  // Verificar si el correo ya está registrado
+  const checkQuery = 'SELECT COUNT(*) AS count FROM usuario WHERE correo = ?';
+  connection.query(checkQuery, [correo], (err, results) => {
     if (err) {
+      // Error de consulta SQL
       return callback(err, null);
     }
-    callback(null, results.insertId); // Devuelve el ID del nuevo registro insertado
+
+    const count = results[0].count;
+    if (count > 0) {
+      // Si ya existe un usuario con este correo, retornar un error
+      return callback(new Error('El correo electrónico ya está registrado'), null);
+    }
+
+    // Si el correo no está registrado, proceder con la inserción del usuario
+    const insertQuery = `
+      INSERT INTO usuario(nombre, correo, contraseña, rol_id, fecha_creacion)
+      VALUES (?, ?, ?, 1, CURRENT_TIMESTAMP)
+    `;
+    connection.query(insertQuery, [nombre, correo, contraseña], (err, results) => {
+      if (err) {
+        // Error al insertar el usuario
+        return callback(err, null);
+      }
+      callback(null, results.insertId); // Devuelve el ID del nuevo registro insertado
+    });
   });
 };
+
+
+// Ruta para registrar usuario
+app.post('/register', (req, res) => {
+  const { nombre, email, contraseña } = req.body;
+
+  // Llama a la función insertUsuarioRegistro para insertar el usuario
+  insertUsuarioRegistro({ nombre, correo: email, contraseña }, (err, userId) => {
+    if (err) {
+      console.error('Error al insertar usuario:', err);
+      if (err.message === 'El correo electrónico ya está registrado') {
+        // Error: Correo electrónico duplicado
+        return res.status(400).json({ message: 'El correo electrónico ya está registrado. Por favor, utiliza otro correo.' });
+      }
+      // Error interno del servidor
+      return res.status(500).json({ message: 'Error interno al registrar usuario. Por favor, inténtalo de nuevo más tarde.' });
+    }
+    // Registro exitoso
+    res.status(200).json({ userId });
+  });
+});
+
+
+
+
+
+
+
 
 
 // Iniciar el servidor
